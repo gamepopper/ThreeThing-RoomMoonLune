929cbe8f78e9386ff0b0e7cae07092907fb66014
Tim Stoddard <info@gamepopper.co.uk>
2015-06-11 07:58:07 -0700

    Stage1 Basis

--- RoomMoonLune/RoomMoonLune/Stage1.cs
+++ RoomMoonLune/RoomMoonLune/Stage1.cs
@@ -1,25 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Ricoh2DFramework;
+using Ricoh2DFramework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
 
 namespace RoomMoonLune
 {
     class Stage1 : IRState
     {
+        Sprite Ship;
+        
+        
         public void Initialize()
         {
             RGlobal.BackgroundColor = Color.CornflowerBlue;
+            RGlobal.Input.InvertedLeft = true;
+            RGlobal.Input.InvertedRight = true;
+            TouchPanel.EnableMouseGestures = true;
+            TouchPanel.EnableMouseTouchPoint = true;
         }
 
         public void LoadContent(ContentManager Content)
         {
-            
+            Ship = new Sprite(Content.Load<Texture2D>("TempShip"));
+            Ship.Position = new Vector2(RGlobal.Resolution.VirtualWidth, RGlobal.Resolution.VirtualHeight) / 2;
+            Ship.Acceleration.Y = 98.1f;
+            Ship.Drag = new Vector2(1.01f, 1);
         }
 
         public void UnloadContent()
@@ -29,12 +37,41 @@ namespace RoomMoonLune
 
         public void Update(GameTime gameTime)
         {
-            
+            Ship.Acceleration.X = RGlobal.Input.LeftAnalogStick.X * 100;
+
+            if (Ship.Acceleration.X < 0)
+            {
+                Ship.SpriteEffects = SpriteEffects.FlipHorizontally;
+            }
+            else if (Ship.Acceleration.X > 0)
+            {
+                Ship.SpriteEffects = SpriteEffects.None;
+            }
+
+            if (RGlobal.Input.isGamePadButtonDown(Buttons.A))
+            {
+                Ship.Velocity.Y -= 5;
+            }
+
+            Ship.Update(gameTime);
+
+            if (Ship.Position.Y + Ship.Collider.Box.Height/2 > RGlobal.Resolution.ScreenHeight)
+            {
+                Ship.Position = new Vector2(Ship.Position.X, RGlobal.Resolution.ScreenHeight - (Ship.Collider.Box.Height / 2));
+                Ship.Velocity.Y = 0;
+                Ship.Drag = new Vector2(1.02f, 1);
+            }
+            else
+            {
+                Ship.Drag = new Vector2(1.001f, 1);
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
+            spriteBatch.Begin();
+            Ship.Draw(spriteBatch);
+            spriteBatch.End();
         }
     }
 }
