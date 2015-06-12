19838a49d899b34eb461ff4babcd3121213fd9fc
Tim Stoddard <info@gamepopper.co.uk>
2015-06-11 19:19:00 -0700

    TransitionState

---
+++ RoomMoonLune/RoomMoonLune/TransitionState.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Ricoh2DFramework;
+
+namespace RoomMoonLune
+{
+    class TransitionState : IRState
+    {
+        StarField starfield;
+
+        public void Initialize()
+        {
+            
+        }
+
+        public void LoadContent(ContentManager Content)
+        {
+            starfield = new StarField(Content.Load<Texture2D>("Star"), 100, 100, new Random());
+        }
+
+        public void UnloadContent()
+        {
+            
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            starfield.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            starfield.Draw(spriteBatch);
+        }
+    }
+}
